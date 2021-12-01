981a837c2a9e1de9d28ad82e3009bde937c058ca
mgmeiner <michael.gmeiner@witt-gruppe.eu>
2021-12-01 19:54:23 +0100

    solve day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,32 @@
 fun main() {
+    fun countIncreases(numbers: List<Int>): Int {
+        var increasedCount = 0
+        var numberBefore = 0
+        for (number in numbers) {
+            if (numberBefore != 0 && number > numberBefore) {
+                increasedCount++
+            }
+
+            numberBefore = number
+        }
+
+        return increasedCount
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return countIncreases(input.toNumbers())
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val numbers = input.toNumbers().windowed(3).map { it.sum() }
+        return countIncreases(numbers)
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
-    val input = readInput("Day01")
+    val input = readInput("Day01_input")
     println(part1(input))
     println(part2(input))
 }
